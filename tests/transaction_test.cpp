7a34f211b17ab0fa30edb9493878985d25ab3c06
Dmitrii <112807714+Dmitriiagishev@users.noreply.github.com>
2023-05-30 16:33:05 +0300

    Update transaction_test.cpp

--- tests/transaction_test.cpp
+++ tests/transaction_test.cpp
@@ -10,29 +10,29 @@ public:
 
 TEST(Transaction, Mock) {
     TransactionMock tr;
-    Account ac1(1, 100);
-    Account ac2(2, 300);
+    Account acc_1(1, 100);
+    Account acc_2(2, 300);
     EXPECT_CALL(tr, Make(testing::_, testing::_, testing::_))
         .Times(5);
     tr.set_fee(200);
-    tr.Make(ac1, ac2, 200);
-    tr.Make(ac2, ac1, 300);
-    tr.Make(ac1, ac1, 0);
-    tr.Make(ac1, ac2, -5);
-    tr.Make(ac2, ac1, 50);
+    tr.Make(acc_1, acc_2, 200);
+    tr.Make(acc_2, acc_1, 300);
+    tr.Make(acc_1, acc_1, 0);
+    tr.Make(acc_1, acc_2, -5);
+    tr.Make(acc_2, acc_1, 50);
 }
 
 TEST(Transaction, SimpleTest) {
     Transaction tr;
-    Account ac1(1, 100);
-    Account ac2(2, 300);
+    Account acc_1(1, 100);
+    Account acc_2(2, 300);
     tr.set_fee(10);
     EXPECT_EQ(tr.fee(), 10);
-    EXPECT_THROW(tr.Make(ac1, ac2, 40), std::logic_error);
-    EXPECT_THROW(tr.Make(ac1, ac2, -5), std::invalid_argument);
-    EXPECT_THROW(tr.Make(ac1, ac1, 100), std::logic_error);
-    EXPECT_FALSE(tr.Make(ac1, ac2, 400));
-    EXPECT_FALSE(tr.Make(ac2, ac1, 300));
-    EXPECT_FALSE(tr.Make(ac2, ac1, 290));
-    EXPECT_TRUE(tr.Make(ac2, ac1, 150));
+    EXPECT_THROW(tr.Make(acc_1, acc_2, 40), std::logic_error);
+    EXPECT_THROW(tr.Make(acc_1, acc_2, -5), std::invalid_argument);
+    EXPECT_THROW(tr.Make(acc_1, acc_1, 100), std::logic_error);
+    EXPECT_FALSE(tr.Make(acc_1, acc_2, 400));
+    EXPECT_FALSE(tr.Make(ac2, acc_1, 300));
+    EXPECT_FALSE(tr.Make(ac2, acc_1, 290));
+    EXPECT_TRUE(tr.Make(ac2, acc_1, 150));
 }
